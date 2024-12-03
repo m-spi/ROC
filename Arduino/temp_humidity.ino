af27556a141419ae0c6095792b937208e1efced1
m-spi <matis.spinelli30@gmail.com>
2024-12-03 14:38:50 +0100

    Arduino Service

--- Arduino/temp_humidity.ino
+++ Arduino/temp_humidity.ino
@@ -1,88 +1,74 @@
-  /*
-    HTS221 - Temp_Humidity with BLE Gesture Control
+/*
+HTS221 - Temp_Humidity with BLE Gesture Control
 
-    This example reads data from the on-board HTS221 sensor of the
-    Nano 33 BLE Sense and prints the temperature and humidity sensor
-    values to the Serial Monitor when they change significantly.
-    Additionally, it handles gesture control over BLE.
+This example reads data from the on-board HTS221 sensor of the
+Nano 33 BLE Sense and prints the temperature and humidity sensor
+values to the Serial Monitor when they change significantly.
+Additionally, it handles gesture control over BLE.
 
-    The circuit:
-    - Arduino Nano 33 BLE Sense
-  */
+The circuit:
+- Arduino Nano 33 BLE Sense
+*/
 
-  #include <Arduino_HTS221.h>
-  #include <ArduinoBLE.h>
+#include <Arduino_HTS221.h>
+#include <ArduinoBLE.h>
 
-  // BLE UUIDs
-  const char* deviceServiceUuid = "185A";
-  const char* deviceServiceCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1214";
+// BLE UUIDs
+const char* deviceServiceUuid = "185A";
+const char* deviceServiceCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1214";
 
-  // Global variables
-  BLEService Temp_service(deviceServiceUuid);
-  BLEFloatCharacteristic Temp_charac(deviceServiceCharacteristicUuid, BLERead);
+// Global variables
+BLEService Temp_service(deviceServiceUuid);
+BLEFloatCharacteristic Temp_charac(deviceServiceCharacteristicUuid, BLERead);
 
-  float old_temp = -1000.0; // Initialize with a large difference
-  float old_hum = -1000.0;  // Initialize with a large difference
+byte manufacturerData[6] = { 1, 2, 3, 4, 5, 6 };
 
-  void setup() {
-    Serial.begin(9600);
-    while (!Serial); // Wait for Serial Monitor to open
+float old_temp = -1000.0;  // Initialize with a large difference
+float old_hum = -1000.0;   // Initialize with a large difference
 
-    // Initialize HTS221 sensor
-    if (!HTS.begin()) {
-      Serial.println("Failed to initialize humidity and temperature sensor!");
-      while (1);
-    }
-
-    // Initialize BLE
-    if (!BLE.begin()) {
-      Serial.println("Failed to start Bluetooth® Low Energy module!");
-      while (1);
-    }
-
-    // Set up BLE device
-    BLE.setLocalName("Arduino Nano 33 BLE (Peripheral)");
-    BLE.setAdvertisedService(Temp_service);
-    Temp_service.addCharacteristic(Temp_charac);
-    BLE.addService(Temp_service);
-    Temp_charac.writeValue(33);
-    BLE.advertise();
-
-    Serial.println("Nano 33 BLE (Peripheral Device) initialized");
-    Serial.println("Waiting for central connection...");
+void setup() {
+  Serial.begin(9600);
+  // Initialize HTS221 sensor
+  if (!HTS.begin()) {
+    while (1);
   }
 
-  void loop() {
-    BLEDevice central = BLE.central();
-    if(central){
-      while(central.connected()){
-        // Read sensor values
-        float temperature = HTS.readTemperature();
-        float humidity = HTS.readHumidity();
-        // Check for significant changes
-        if (abs(old_temp - temperature) >= 0.5 || abs(old_hum - humidity) >= 1) {
-          old_temp = temperature;
-          old_hum = humidity;
+  // Initialize BLE
+  if (!BLE.begin()) {
+    while (1);
+  }
 
-          // Print updated sensor values
-          Serial.print("Temperature = ");
-          Serial.print(temperature);
-          Serial.println(" °C");
-          Serial.print("Humidity    = ");
-          Serial.print(humidity);
-          Serial.println(" %");
-          Serial.println();
+  // Set up BLE device
+  BLE.setLocalName("ROC0");
 
-          
-              // Loop while central is connected
-            Temp_charac.writeValue(old_temp);
-            Serial.print(old_temp);
-        }
+  // Data to recognise the device
+  BLE.setManufacturerData(manufacturerData, 6);
 
+  // Add characteristic
+  Temp_service.addCharacteristic(Temp_charac);
+  BLE.addService(Temp_service);
+  //BLE.setAdvertisedService(Temp_service);
+  //Temp_charac.writeValue(33);
+  //BLE.setConnectionInterval(0x0006, 0x0c80);
+
+  BLE.setConnectable(true);
+  BLE.advertise();
+}
+
+void loop() {
+  BLEDevice central = BLE.central();
+  if (central) {
+    while (central.connected()) {
+      // Read sensor values
+      float temperature = HTS.readTemperature();
+      float humidity = HTS.readHumidity();
+      // Check for significant changes
+      if (abs(old_temp - temperature) >= 0.5 || abs(old_hum - humidity) >= 1) {
+        old_temp = temperature;
+        old_hum = humidity;
+
+        Temp_charac.writeValue(old_temp);
       }
     }
   }
-
-
-
-
+}
--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+import subprocess
 from flask import Flask, render_template, request, jsonify
 #from database_final import create_table, insert_data, get_sensor_data
 import json
 
+JSON_FILE = "data_pipe.json"
+subprocess.Popen(["sudo", "./.venv/bin/python", "./ble.py"])
+
 app = Flask(__name__)
 
 #create_table()
-
-JSON_FILE = "sensor_data.json"
 
 def read_json():
     try:
@@ -17,37 +19,35 @@ def read_json():
     except json.JSONDecodeError:
         return []  # Retourner une liste vide si le contenu est invalide
         
-def write_json(data):
-    try:
-        with open(JSON_FILE, "r") as file:
-            json.dump(data, file, indent=4)
-    except Exception as e:
-        print(f"Erreur lors de l'écriture du fichier JSON : {e}")
-
-
+#def write_json(data):
+#    try:
+#        with open(JSON_FILE, "r") as file:
+#            json.dump(data, file, indent=4)
+#    except Exception as e:
+#        print(f"Erreur lors de l'écriture du fichier JSON : {e}")
 
 @app.route('/')
 def home():
     sensor_data = read_json()
     return render_template('index.html', sensor_data=sensor_data)
 
-@app.route('/update-sensor', methods=['POST'])
-def update_sensor():
-    try:
-        # Récupérer les données JSON envoyées par la Raspberry Pi
-        data = request.json
-        if not data:
-            return jsonify({'status': 'error', 'message': 'Aucune donnée reçue'}), 400
-        
-        # Sauvegarder les données dans la base de données
-        sensor_data = read_json()
-        sensor_data.append(data)
-        write_json(sensor_data)
-        
-        return jsonify({'status': 'success'})
-    except Exception as e:
-        print(f"Erreur lors du traitement : {e}")
-        return jsonify({'status': 'error', 'message': 'Erreur serveur'}), 500
+#@app.route('/update-sensor', methods=['POST'])
+#def update_sensor():
+#    try:
+#        # Récupérer les données JSON envoyées par la Raspberry Pi
+#        data = request.json
+#        if not data:
+#            return jsonify({'status': 'error', 'message': 'Aucune donnée reçue'}), 400
+#        
+#        # Sauvegarder les données dans la base de données
+#        sensor_data = read_json()
+#        sensor_data.append(data)
+#        write_json(sensor_data)
+#        
+#        return jsonify({'status': 'success'})
+#    except Exception as e:
+#        print(f"Erreur lors du traitement : {e}")
+#        return jsonify({'status': 'error', 'message': 'Erreur serveur'}), 500
 
 @app.route('/get-sensor-data', methods=['GET'])
 def get_sensor_data_route():
@@ -60,12 +60,5 @@ def get_sensor_data_route():
          print(f"Erreur lors de la récupération des données : {e}")
          return jsonify({'status': 'error', 'message': 'Erreur serveur'}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
-
-
-
-
-
-
-
+#if __name__ == '__main__':
+#    app.run(debug=False, host='0.0.0.0', port=5000)
